3dc8f709cfd1f0414b5461a40536af919031326e
panjisantoso <panjisantoso20@gmail.com>
2021-02-03 20:25:53 +0800

    textarea isi pengumuman

--- resources/views/pengumuman/detail.blade.php
+++ resources/views/pengumuman/detail.blade.php
@@ -23,23 +23,32 @@
     <section class="inner-page">
       <div class="container">
         <div class="owl-carousel portfolio-details-carousel">
+          @if(!empty($pengumumans->gambar1))
             <img src="/{{ $pengumumans->gambar1 }}" style="width:400px;">
+          @endif
+          @if(!empty($pengumumans->gambar2))
             <img src="/{{ $pengumumans->gambar2 }}" style="width:400px;">
+          @endif
+          @if(!empty($pengumumans->gambar3))
             <img src="/{{ $pengumumans->gambar3 }}" style="width:400px;">
+          @endif 
         </div>
+        <br>
         <p align="justify">
-            {{ $pengumumans->isi }}
+          <textarea readonly rows="30" cols="47"> {{$pengumumans->isi}}</textarea>            
         </p>
         <br>
         
         <br>
+        @if(!empty($pengumumans->download))
         <h3>Download File</h3>
         <p>
             Berikut merupakan beberapa file yang dapat diunduh dari Pengumuman ini<br>
-            @if(!empty($pengumumans->download))
+            
                 <a href="/{{ $pengumumans->download }}" class="btn btn-primary" download>Download File</a>
-            @endif
+            
         </p>
+        @endif
       </div>
     </section>
 
